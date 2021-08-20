612b14e3219d4293dcb54f8e0cfb9fa3517891dd
Edwin <ribeiro.edwin@gmail.com>
2021-08-20 11:22:07 +0200

    Setting up NavGraph & Hilt

--- app/src/main/java/com/somecompose/composepokeapp/MainActivity.kt
+++ app/src/main/java/com/somecompose/composepokeapp/MainActivity.kt
@@ -7,32 +7,53 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.somecompose.composepokeapp.ui.theme.ComposePokeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePokeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                //Inst Ref
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "poke_list_screen"
+                ) {
+                    composable("poke_list_screen") {
+
+                    }
+                    composable(
+                        "poke_detail_screen/{dominantColor}/{pokeName}",
+                        arguments = listOf(
+                            navArgument("dominantColor") {
+                                type = NavType.IntType
+                            },
+                            navArgument("pokeName") {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokeName = remember {
+                            it.arguments?.getString("pokeName")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposePokeAppTheme {
-        Greeting("Android")
     }
 }
